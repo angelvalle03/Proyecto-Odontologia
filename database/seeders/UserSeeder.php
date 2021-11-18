e9c83c977e50eda08754a14ee513f3baa118e8c7
angel-valle <angelvalle2000@hotmail.com>
2021-11-18 16:12:21 -0500

    Asignacion de roles manual

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -20,30 +20,30 @@ class UserSeeder extends Seeder
             'name'=>'Angel Valle',
             'email'=>'angelvalle172@gmail.com',
             'password'=> bcrypt('Odonto-angel')
-        ])->assignRole('admin');
+        ])->assignRole('Admin');
 
         User::create([
             'name'=>'Jorge valdelamar',
             'email'=>'jorge123@gmail.com',
             'password'=> bcrypt('Odonto-jorge')
-        ]);
+        ])->assignRole('Admin');;
 
         User::create([
             'name'=>'Yiseth Delgado',
             'email'=>'yiseth123@gmail.com',
             'password'=> bcrypt('Odonto-yise')
-        ]);
+        ])->assignRole('Doctor');;
 
         User::create([
             'name'=>'Daniel Baldiris',
             'email'=>'daniel123@gmail.com',
             'password'=> bcrypt('Odonto-daniel')
-        ]);
+        ])->assignRole('Doctor');;
 
         User::create([
             'name'=>'Jhon Herrera',
             'email'=>'jhona123@gmail.com',
             'password'=> bcrypt('Odonto-jhon')
-        ]);
+        ])->assignRole('Paciente');;
     }
 }
